ab5ed2dfb836f176f30037e177f27f1ed876c27c
acopero <acopero@soamee.com>
2019-04-25 18:55:39 +0200

    chore(): create UsersList screen

---
+++ src/screens/UsersListScreen/view.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+import { Table, Avatar } from 'antd';
+
+const { Column, ColumnGroup } = Table;
+
+const UsersListScreen = ({ fetchUsers, users }) => {
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  return (
+    <Table dataSource={users}>
+      <Column
+        title="Avatar"
+        dataIndex="picture.thumbnail"
+        key="avatar"
+        render={picture => <Avatar src={picture} />}
+      />
+      <ColumnGroup title="Name">
+        <Column title="First Name" dataIndex="name.first" key="firstName" />
+        <Column title="Last Name" dataIndex="name.last" key="lastName" />
+      </ColumnGroup>
+      <Column title="Age" dataIndex="dob.age" key="age" />
+      <Column title="Address" dataIndex="location.street" key="address" />
+      <Column title="Email" dataIndex="email" key="email" />
+      <Column title="Phone" dataIndex="phone" key="phone" />
+    </Table>
+  );
+};
+
+UsersListScreen.propTypes = {
+  fetchUsers: PropTypes.func.isRequired,
+  users: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+};
+
+export default UsersListScreen;
