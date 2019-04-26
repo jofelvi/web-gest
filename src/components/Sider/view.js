26ddbd67113f27722149be337d4792b35b6fc28f
acopero <acopero@soamee.com>
2019-04-26 14:58:50 +0200

    chore(): add menus to sider

--- src/components/Sider/view.js
+++ src/components/Sider/view.js
@@ -8,7 +8,7 @@ const { SubMenu, Item } = Menu;
 const { Sider: AntdSider } = Layout;
 
 const Sider = () => (
-  <AntdSider width={200} style={{ background: '#fff' }}>
+  <AntdSider width={235} style={{ background: '#fff' }}>
     <Menu
       mode="inline"
       defaultSelectedKeys={['1']}
@@ -17,29 +17,142 @@ const Sider = () => (
       <Item key="1">
         <Icon type="desktop" />
         <span>
-          <SideLink to="/">Home</SideLink>
+          <SideLink to="/">Inicio</SideLink>
         </span>
       </Item>
       <Item key="2">
         <Icon type="user" />
         <span>
-          <SideLink to="/users">Users</SideLink>
+          <SideLink to="/users">Usuarios Farmacia</SideLink>
         </span>
       </Item>
       <SubMenu
-        key="sub3"
+        key="sub1"
         title={
           <span>
-            <Icon type="notification" />
-            subnav 3
+            <Icon type="setting" />
+            <span>Gestión</span>
           </span>
         }
       >
-        <Menu.Item key="9">option9</Menu.Item>
-        <Menu.Item key="10">option10</Menu.Item>
-        <Menu.Item key="11">option11</Menu.Item>
-        <Menu.Item key="12">option12</Menu.Item>
+        <Item key="3">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/group">Grupo</SideLink>
+          </span>
+        </Item>
+        <Item key="4">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/enterprise-group">
+              Grupo Empresarial
+            </SideLink>
+          </span>
+        </Item>
+        <Item key="5">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/delegate">Delegado</SideLink>
+          </span>
+        </Item>
+        <Item key="6">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/area-managers">Jefes de zona</SideLink>
+          </span>
+        </Item>
+        <Item key="7">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/products">Producto</SideLink>
+          </span>
+        </Item>
+        <Item key="8">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/subgroup">Subgrupo</SideLink>
+          </span>
+        </Item>
+        <Item key="9">
+          <span>
+            <SideLink to="/management/foreclosed-orders">
+              Pedidos embargados
+            </SideLink>
+          </span>
+        </Item>
+        <Item key="10">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/management/commercial-deals">
+              Acuerdos comerciales
+            </SideLink>
+          </span>
+        </Item>
       </SubMenu>
+      <Item key="11">
+        <Icon type="user" />
+        <span>
+          <SideLink to="/wholesaler-pharmacy">Mayorista-Farmacia</SideLink>
+        </span>
+      </Item>
+      <Item key="12">
+        <Icon type="user" />
+        <span>
+          <SideLink to="/deals">Ofertas</SideLink>
+        </span>
+      </Item>
+      <SubMenu
+        key="sub2"
+        title={
+          <span>
+            <Icon type="setting" />
+            <span>Gestión masiva</span>
+          </span>
+        }
+      >
+        <Item key="13">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/massive-management/delegates-pharmacy">
+              Delegados y Farmacias
+            </SideLink>
+          </span>
+        </Item>
+        <Item key="14">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/massive-management/relationships">
+              Relaciones
+            </SideLink>
+          </span>
+        </Item>
+        <Item key="15">
+          <Icon type="user" />
+          <span>
+            <SideLink to="/massive-management/purchase-limit">
+              Limitación de compras
+            </SideLink>
+          </span>
+        </Item>
+      </SubMenu>
+      <Item key="16">
+        <Icon type="user" />
+        <span>
+          <SideLink to="/cmi">CMI</SideLink>
+        </span>
+      </Item>
+      <Item key="17">
+        <Icon type="user" />
+        <span>
+          <SideLink to="/reports">Informes</SideLink>
+        </span>
+      </Item>
+      <Item key="18">
+        <Icon type="user" />
+        <span>
+          <SideLink to="/online-help">Ayuda Online</SideLink>
+        </span>
+      </Item>
     </Menu>
   </AntdSider>
 );
