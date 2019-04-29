c484964cba8c97b74bc3b5e057762167847b2f4e
acopero <acopero@soamee.com>
2019-04-29 14:57:16 +0200

    chore(): create auth's actions and actionTypes

---
+++ src/modules/auth/actions.js
@@ -0,0 +1,53 @@
+import { createActions } from 'redux-actions';
+
+import {
+  CHECK_LOGIN,
+  CHECK_LOGIN_SUCCESS,
+  CHECK_LOGIN_FAILED,
+  LOGIN,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  LOGOUT,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAILED,
+  REFRESH_TOKEN,
+  REFRESH_TOKEN_SUCCESS,
+  REFRESH_TOKEN_FAILED,
+  SET_TOKEN,
+  FETCH_ME,
+  SET_ME
+} from './actionTypes';
+
+export const {
+  checkLogin,
+  checkLoginSuccess,
+  checkLoginFailed,
+  login,
+  loginSuccess,
+  loginFailed,
+  logout,
+  logoutSuccess,
+  logoutFailed,
+  refreshToken,
+  refreshTokenSuccess,
+  refreshTokenFailed,
+  setToken,
+  fetchMe,
+  setMe
+} = createActions(
+  CHECK_LOGIN,
+  CHECK_LOGIN_SUCCESS,
+  CHECK_LOGIN_FAILED,
+  LOGIN,
+  LOGIN_SUCCESS,
+  LOGIN_FAILED,
+  LOGOUT,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAILED,
+  REFRESH_TOKEN,
+  REFRESH_TOKEN_SUCCESS,
+  REFRESH_TOKEN_FAILED,
+  SET_TOKEN,
+  FETCH_ME,
+  SET_ME
+);
