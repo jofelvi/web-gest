5647419788bbd8e11b9ef357189bea739fa5cb07
acopero <acopero@soamee.com>
2019-04-30 13:28:03 +0200

    chore(): add login check and remove sider if not logged

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+import { checkLogin } from './modules/auth/actions';
 
-import { Layout } from "antd";
+import { Layout } from 'antd';
 
-import { Switch, Route, withRouter } from "react-router-dom";
+import { Switch, Route, withRouter } from 'react-router-dom';
 
-import PrivateRoute from "./components/PrivateRoute";
-import Header from "./components/Header";
-import Sider from "./components/Sider";
+import PrivateRoute from './components/PrivateRoute';
+import Header from './components/Header';
+import Sider from './components/Sider';
 
-import HomeScreen from "./screens/HomeScreen";
-import UsersListScreen from "./screens/UsersListScreen";
-import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from './screens/HomeScreen';
+import UsersListScreen from './screens/UsersListScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const { Content } = Layout;
 
-const App = () => (
+const App = ({ location: { pathname } }) => (
   <Layout>
     <Header className="header" />
     <Layout>
-      <Sider />
-      <Layout style={{ padding: "0 24px 24px" }}>
+      {pathname !== '/login' ? <Sider /> : null}
+      <Layout>
         <Content
           style={{
-            background: "#fff",
+            background: '#fff',
             padding: 24,
             margin: 0,
             minHeight: 280
@@ -46,8 +47,4 @@ App.propTypes = {
   status: PropTypes.string.isRequired
 };
 
-export default withRouter(
-  connect(state => ({
-    status: state.auth.status
-  }))(App)
-);
+export default withRouter(connect(() => ({}))(App));
