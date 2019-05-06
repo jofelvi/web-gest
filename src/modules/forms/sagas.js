3228e12dc5dd681b7d83de8d66cee032c76d5fe2
acopero <acopero@soamee.com>
2019-05-06 13:48:48 +0200

    chore(): create forms reducers, sagas and add it to rootReducers and rootSagas

---
+++ src/modules/forms/sagas.js
@@ -0,0 +1,136 @@
+import { takeLatest, call, put, select } from 'redux-saga/effects';
+
+import {
+  startProcessFailed,
+  startProcessSuccess,
+  continueProcess as nextProcess,
+  continueProcessSuccess,
+  startProcess,
+  completeTaskSuccess,
+  getTaskVariablesFailed,
+  getTaskVariablesSuccess,
+  complete
+} from './actions';
+
+import {
+  START_PROCESS,
+  CONTINUE_PROCESS,
+  COMPLETE_TASK,
+  GET_TASK_VARIABLES
+} from './actionTypes';
+
+import * as api from './api';
+import { login } from '../auth/actions';
+
+function* startProcessSaga({ payload }) {
+  try {
+    const response = yield call(api.startProcess, payload.key);
+    if (response.data !== null) {
+      yield put(
+        startProcessSuccess({
+          key: payload.key,
+          taskName: response.data,
+          process: payload.key
+        })
+      );
+    } else {
+      yield put(nextProcess({ variables: [] }));
+    }
+  } catch (e) {
+    console.error(e);
+    yield put(startProcessFailed());
+    const user = process.env.REACT_APP_ANONYM_USER;
+    const password = process.env.REACT_APP_ANONYM_PASSWORD;
+    console.warn('login with anonym user');
+    yield put(
+      login({
+        values: { user, password },
+        nextAction: startProcess,
+        nextActionPayload: payload
+      })
+    );
+  }
+}
+
+export function* watchStartProcess() {
+  yield takeLatest(START_PROCESS, startProcessSaga);
+}
+
+function* continueProcess({ payload }) {
+  const processKey = yield select(state => state.forms.processKey);
+  let response = yield call(api.continueProcess, processKey, payload.variables);
+  const procId = response.data;
+  response = yield call(api.checkTask, procId);
+  if (response.status === 200) {
+    const newTaskName = response.data.formKey;
+    yield put(
+      continueProcessSuccess({
+        taskName: newTaskName,
+        taskId: response.data.taskId
+      })
+    );
+    window.history.pushState(
+      null,
+      null,
+      `/process/${processKey}/${response.data.formKey}`
+    );
+  } else {
+    console.log('---> completed');
+  }
+}
+
+export function* watchContinueProcess() {
+  yield takeLatest(CONTINUE_PROCESS, continueProcess);
+}
+
+function* completeTaskProcess({ payload }) {
+  const processKey = yield select(state => state.forms.processKey);
+  const taskId = yield select(state => state.forms.taskId);
+  let response;
+  console.log({ taskId });
+  if (taskId) {
+    response = yield call(api.completeTask, taskId, payload.variables);
+  } else {
+    response = yield call(api.continueProcess, processKey, payload.variables);
+  }
+  const procId = response.data;
+  console.log({ responseComplete: response });
+  response = yield call(api.checkTask, procId);
+  console.log({ responseNext: response });
+  if (response.status === 200) {
+    const newTaskName = response.data.formKey;
+    yield put(
+      completeTaskSuccess({
+        taskName: newTaskName,
+        taskId: response.data.taskId
+      })
+    );
+    window.history.pushState(
+      null,
+      null,
+      `/process/${processKey}/${response.data.formKey}`
+    );
+  } else {
+    console.log('--> completed');
+    yield put(complete());
+  }
+}
+
+export function* watchCompleteTaskProcess() {
+  yield takeLatest(COMPLETE_TASK, completeTaskProcess);
+}
+
+function* getTaskVariables() {
+  try {
+    const taskId = yield select(state => state.forms.taskId);
+    const response = yield call(api.getTaskVariables, taskId);
+    yield put(getTaskVariablesSuccess(response.data));
+  } catch (e) {
+    console.error(e);
+    yield put(getTaskVariablesFailed());
+  }
+}
+
+export function* watchGetTaskVariables() {
+  yield takeLatest(GET_TASK_VARIABLES, getTaskVariables);
+}
--- src/modules/rootReducers.js
+++ src/modules/rootReducers.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import auth from './auth/reducers';
 import users from './users/reducers';
 import tasks from './tasks/reducers';
+import forms from './forms/reducers';
 
 export default combineReducers({
   auth,
   users,
-  tasks
+  tasks,
+  forms
 });
