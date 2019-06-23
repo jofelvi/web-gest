4b1a04b43da8c31c73380f206a16130453422b38
acopero <acopero@soamee.com>
2019-06-23 11:15:15 +0200

    chore(): create some styles and add it to LoginForm component

--- src/components/LoginForm/view.js
+++ src/components/LoginForm/view.js
@@ -5,8 +5,9 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 
 import { Form, Input, Icon, Button } from 'antd';
+import { Link } from 'react-router-dom';
 
-import { ButtonContainer } from './styles';
+import { ButtonContainer, SignupContainer } from './styles';
 
 const { Item } = Form;
 
@@ -16,52 +17,57 @@ const validationSchema = Yup.object().shape({
 });
 
 const LoginForm = ({ login }) => (
-  <Formik
-    initialValues={{
-      user: '',
-      password: ''
-    }}
-    validationSchema={validationSchema}
-    onSubmit={values => {
-      login({ values: { user: values.user, password: values.password } });
-    }}
-  >
-    {({ values, handleChange, handleBlur, handleSubmit }) => (
-      <>
-        <Item>
-          <Input
-            id="user"
-            value={values.user}
-            prefix={<Icon type="user" />}
-            placeholder="User"
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-        </Item>
-        <Item>
-          <Input
-            id="password"
-            value={values.password}
-            prefix={<Icon type="lock" />}
-            type="password"
-            placeholder="Password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-        </Item>
-        <ButtonContainer>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="login-form-button"
-            onClick={handleSubmit}
-          >
-            Log in
-          </Button>
-        </ButtonContainer>
-      </>
-    )}
-  </Formik>
+  <>
+    <Formik
+      initialValues={{
+        user: '',
+        password: ''
+      }}
+      validationSchema={validationSchema}
+      onSubmit={values => {
+        login({ values: { user: values.user, password: values.password } });
+      }}
+    >
+      {({ values, handleChange, handleBlur, handleSubmit }) => (
+        <>
+          <Item>
+            <Input
+              id="user"
+              value={values.user}
+              prefix={<Icon type="user" />}
+              placeholder="User"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Item>
+          <Item>
+            <Input
+              id="password"
+              value={values.password}
+              prefix={<Icon type="lock" />}
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+          </Item>
+          <ButtonContainer>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              onClick={handleSubmit}
+            >
+              Log in
+            </Button>
+          </ButtonContainer>
+        </>
+      )}
+    </Formik>
+    <SignupContainer>
+      ¿No tienes cuenta? <Link to="/signup">Regístrate</Link>
+    </SignupContainer>
+  </>
 );
 
 LoginForm.propTypes = {
