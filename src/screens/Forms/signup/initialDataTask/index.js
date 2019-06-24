4becc36bc250ba2941502ca9431818d9f93a9831
acopero <acopero@soamee.com>
2019-06-24 20:42:49 +0200

    feat(): get task variables and add it to form

--- src/screens/Forms/signup/initialDataTask/index.js
+++ src/screens/Forms/signup/initialDataTask/index.js
@@ -1,10 +1,16 @@
 import { connect } from 'react-redux';
 
-import { completeTask } from '../../../../modules/forms/actions';
+import {
+  getTaskVariables,
+  completeTask
+} from '../../../../modules/forms/actions';
 
 import View from './view';
 
 export default connect(
-  () => ({}),
-  { completeTask }
+  state => ({
+    completed: state.forms.completed,
+    taskVariables: state.forms.taskVariables
+  }),
+  { getTaskVariables, completeTask }
 )(View);
--- src/screens/Forms/signup/initialDataTask/view.js
+++ src/screens/Forms/signup/initialDataTask/view.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as Yup from 'yup';
 import { Formik } from 'formik';
@@ -12,92 +13,108 @@ const validationSchema = Yup.object().shape({
   acceptTerms: Yup.bool().required()
 });
 
-const InitialDataTask = ({ completeTask }) => (
-  <Row type="flex" justify="center">
-    <Formik
-      initialValues={{
-        user: '',
-        nif: '',
-        isDemo: false,
-        acceptTerms: false
-      }}
-      onSubmit={values => {
-        const variables = [];
-        Object.entries(values).map(value => {
-          const taskVariable = {
-            name: value[0],
-            value: value[1],
-            type: `${(typeof value[1])
-              .charAt(0)
-              .toUpperCase()}${(typeof value[1]).slice(1)}`,
-            transient: false
-          };
-          return variables.push(taskVariable);
-        });
-        completeTask({ variables });
-      }}
-      validationSchema={validationSchema}
-    >
-      {({ values, handleSubmit, handleChange, handleBlur }) => (
-        <Col span={12}>
-          <Form onSubmit={handleSubmit}>
-            <Row type="flex" justify="center">
-              <Col span={12}>
-                <Form.Item label="Usuario">
-                  <Input
-                    value={values.user}
-                    onChange={handleChange('user')}
-                    onBlur={handleBlur('user')}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row type="flex" justify="center">
-              <Col span={12}>
-                <Form.Item label="NIF">
-                  <Input
-                    value={values.nif}
-                    onChange={handleChange('nif')}
-                    onBlur={handleBlur('nif')}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row type="flex" justify="center">
-              <Col span={12}>
-                <Form.Item label="Solicito periodo de Demo">
-                  <Checkbox
-                    value={values.isDemo}
-                    onChange={handleChange('isDemo')}
-                    onBlur={handleBlur('isDemo')}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row type="flex" justify="center">
-              <Col span={12}>
-                <Form.Item label="Acepto las condiciones legales">
-                  <Checkbox
-                    value={values.acceptTerms}
-                    onChange={handleChange('acceptTerms')}
-                    onBlur={handleBlur('acceptTerms')}
-                  />
-                </Form.Item>
-              </Col>
-            </Row>
-            <Row type="flex" justify="center">
-              <Col span={12}>
-                <Button type="primary" htmlType="submit">
-                  Enviar
-                </Button>
-              </Col>
-            </Row>
-          </Form>
-        </Col>
-      )}
-    </Formik>
-  </Row>
-);
+const InitialDataTask = ({
+  getTaskVariables,
+  taskVariables,
+  completeTask,
+  completed
+}) => {
+  useEffect(() => {
+    getTaskVariables();
+  }, [getTaskVariables]);
+
+  return completed ? (
+    <Redirect to="/task/completed" />
+  ) : (
+    <Row type="flex" justify="center">
+      <Formik
+        initialValues={{
+          user: taskVariables && taskVariables[0] ? taskVariables[0].value : '',
+          nif: taskVariables && taskVariables[1] ? taskVariables[1].value : '',
+          isDemo:
+            taskVariables && taskVariables[2] ? taskVariables[2].value : '',
+          acceptTerms:
+            taskVariables && taskVariables[3] ? taskVariables[3].value : ''
+        }}
+        onSubmit={values => {
+          const variables = [];
+          Object.entries(values).map(value => {
+            const taskVariable = {
+              name: value[0],
+              value: value[1],
+              type: `${(typeof value[1])
+                .charAt(0)
+                .toUpperCase()}${(typeof value[1]).slice(1)}`,
+              transient: false
+            };
+            return variables.push(taskVariable);
+          });
+          console.log(completed);
+          completeTask({ variables });
+        }}
+        validationSchema={validationSchema}
+      >
+        {({ values, handleSubmit, handleChange, handleBlur }) => (
+          <Col span={12}>
+            <Form onSubmit={handleSubmit}>
+              <Row type="flex" justify="center">
+                <Col span={12}>
+                  <Form.Item label="Usuario">
+                    <Input
+                      value={values.user}
+                      onChange={handleChange('user')}
+                      onBlur={handleBlur('user')}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col span={12}>
+                  <Form.Item label="NIF">
+                    <Input
+                      value={values.nif}
+                      onChange={handleChange('nif')}
+                      onBlur={handleBlur('nif')}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col span={12}>
+                  <Form.Item label="Solicito periodo de Demo">
+                    <Checkbox
+                      value={values.isDemo}
+                      onChange={handleChange('isDemo')}
+                      onBlur={handleBlur('isDemo')}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col span={12}>
+                  <Form.Item label="Acepto las condiciones legales">
+                    <Checkbox
+                      value={values.acceptTerms}
+                      onChange={handleChange('acceptTerms')}
+                      onBlur={handleBlur('acceptTerms')}
+                    />
+                  </Form.Item>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col span={12}>
+                  <Button type="primary" htmlType="submit">
+                    Enviar
+                  </Button>
+                </Col>
+              </Row>
+            </Form>
+          </Col>
+        )}
+      </Formik>
+    </Row>
+  );
+};
 
 InitialDataTask.propTypes = {
   completeTask: PropTypes.func.isRequired
