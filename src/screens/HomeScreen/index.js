8d2fe77a381b756da16338e46177a84d49701940
acopero <acopero@soamee.com>
2019-07-04 13:35:01 +0200

    feat(): fetch task when requested

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 
+import { fetchTaskForm } from '../../modules/tasks/actions';
+
 import View from './view';
 
 export default connect(
   state => ({
     process: state.forms.process,
     taskName: state.forms.taskName,
-    taskId: state.forms.taskId
+    taskId: state.forms.taskId,
   }),
-  {}
+  { fetchTaskForm }
 )(View);
