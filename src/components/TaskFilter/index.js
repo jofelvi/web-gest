53e2ec275c27c6223f64c8fb165cd659513c3706
acopero <acopero@soamee.com>
2019-07-09 09:55:50 +0200

    fix(): remove isFetching and fix eslint errors

--- src/components/TaskFilter/index.js
+++ src/components/TaskFilter/index.js
@@ -5,6 +5,6 @@ import { setTaskListFilter, fetchTaskList } from '../../modules/tasks/actions';
 import View from './view';
 
 export default connect(
-  state => ({ sortBy: state.tasks.sortBy, isFetching: state.tasks.isFetching }),
+  state => ({ sortBy: state.tasks.sortBy }),
   { setTaskListFilter, fetchTaskList }
 )(View);
