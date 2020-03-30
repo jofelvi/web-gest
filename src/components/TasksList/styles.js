5bc225aaf1b76faad4c3a27f7d18c93579c7daf8
Lucía Martín <lmartin@soamee.com>
2020-03-30 13:25:58 +0000

    Update styles.js

--- src/components/TasksList/styles.js
+++ src/components/TasksList/styles.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 import { Row, Col } from 'antd';
 
 export const TasksListRow = styled(Row)`
-overflow: auto;
+    overflow: auto;
     max-height: 100vh;
 `;
 export const TaskListCol = styled(Col)`
-overflow: scroll;
+    overflow: scroll;
 `;
