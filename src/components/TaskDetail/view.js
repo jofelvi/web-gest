4142c8b8c2aec7dd556dc6e5ed3cd6cf28f8ae64
Lucia Martin <luciamartin@Lucias-MacBook-Pro.local>
2020-04-02 15:20:25 +0200

    placeholder for textarea added

--- src/components/TaskDetail/view.js
+++ src/components/TaskDetail/view.js
@@ -4,13 +4,13 @@ import InputDataButton from '../InputDataButton'
 import { Input, Button } from 'antd';
 import TabsTaskDetail from '../TabsTaskDetail'
 import ModalTaskDetail from '../ModalTaskDetail'
-import { 
-  Container, 
-  CardCustom, 
-  ContainerSide, 
-  ContainerTextArea, 
-  ContainerTabs, 
-  Label, 
+import {
+  Container,
+  CardCustom,
+  ContainerSide,
+  ContainerTextArea,
+  ContainerTabs,
+  Label,
   ContainerInputData,
   ContainerModal,
   ContentContainer
@@ -22,11 +22,11 @@ const { TextArea } = Input;
 
 const TaskDetail = ({
   history,
-  selectedTask: { 
-    id, 
-    processInstanceId, 
-    name, 
-    processDefinitionName, 
+  selectedTask: {
+    id,
+    processInstanceId,
+    name,
+    processDefinitionName,
     assignee,
     due,
     created,
@@ -34,87 +34,87 @@ const TaskDetail = ({
 }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [inputKey, setInputKey] = useState('');
-  
+
   const showModal = () => {
-   setIsVisible(true)   
+    setIsVisible(true)
   };
 
   const handleOk = e => {
     setIsVisible(false)
   };
-  
+
   const handleCancel = e => {
     setIsVisible(false)
   };
- 
-  return(
+
+  return (
     <CardCustom title={name} bordered={false} >
       <Container>
         <ContainerInputData>
-          <EditButtons  
-            setInputKey = {setInputKey}
-            showModal = {showModal}
-            due = { due ? due : '09/08/2020' } 
-            assignee = { assignee ? assignee :'ADMIN' }
-            priority = { priority ? priority : 'no-data' }
-            created = { created }>
+          <EditButtons
+            setInputKey={setInputKey}
+            showModal={showModal}
+            due={due ? due : '09/08/2020'}
+            assignee={assignee ? assignee : 'ADMIN'}
+            priority={priority ? priority : 'no-data'}
+            created={created}>
           </EditButtons>
         </ContainerInputData>
         <ContainerTextArea>
           <Label>Comentario</Label>
-          <TextArea placeholder = {'Solicitud de nueva entidad'} rows={4} />
+          <TextArea placeholder={'Solicitud de nueva entidad'} rows={4} />
         </ContainerTextArea>
         <ContainerTabs>
           <TabsTaskDetail>
           </TabsTaskDetail>
         </ContainerTabs>
         <ContainerModal>
-          <Formik 
+          <Formik
             initialValues={{
-            due: '',
-            assignee: '',
-            priority: ''
+              due: '',
+              assignee: '',
+              priority: ''
             }}
             validationSchema={validationSchema}
             onSubmit={(values) => {
-              
+
             }}>
             {({
-                values,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-                errors,
-                touched,
-              }) => (
+              values,
+              handleChange,
+              handleBlur,
+              handleSubmit,
+              errors,
+              touched,
+            }) => (
                 <ModalTaskDetail
-                  visible = { isVisible }
-                  handleCancel = { handleCancel }
-                  titleModal = { mapperInputData.map(input =>(input[inputKey])) }
+                  visible={isVisible}
+                  handleCancel={handleCancel}
+                  titleModal={mapperInputData.map(input => (input[inputKey]))}
                   footer={[
-                    <Button 
-                      key="back" 
+                    <Button
+                      key="back"
                       onClick={handleCancel}>
                       Atrás
                     </Button>,
-                    <Button 
-                      key="submit" 
-                      type="primary"  
+                    <Button
+                      key="submit"
+                      type="primary"
                       onClick={(e) => {
                         handleSubmit(e);
                         handleOk(e);
                       }}>
                       Guardar
                     </Button>,
-                    ]}
-                  content= {
+                  ]}
+                  content={
                     <ContentContainer>
-                      <Label>{ mapperInputData.map(input =>(input[inputKey])) }
+                      <Label>{mapperInputData.map(input => (input[inputKey]))}
                       </Label>
                       <Input
-                        id= {inputKey}
+                        id={inputKey}
                         value={values[inputKey]}
-                        placeholder= { mapperInputData.map(input =>(input[inputKey])) }
+                        placeholder={mapperInputData.map(input => (input[inputKey]))}
                         onChange={handleChange}
                         onBlur={handleBlur}
                       />
@@ -126,8 +126,8 @@ const TaskDetail = ({
           </Formik>
         </ContainerModal>
       </Container>
-    </CardCustom> 
-  ) 
+    </CardCustom>
+  )
 };
 
 TaskDetail.propTypes = {
