729ffec71a5380f5350e556af97f3afb5b729803
IvanG <ivan.g@onthegosystems.com>
2020-12-01 19:23:12 +0100

    [NTR-280] fixes

--- src/components/InfoCardOrder/view.js
+++ src/components/InfoCardOrder/view.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Tooltip } from 'antd';
 
 import PropTypes from 'prop-types';
 
@@ -33,97 +33,109 @@ setLoading,
 loading,
 deleteOrderLineById,
 deleteOrderLineSetLoading,
-}) => (
-    <div>
-      <InfoContainer>
-        <DescriptionContainer>
-        <Descriptions title="Datos Pedido" layout="horizontal">
-          <Descriptions.Item label="Num. Pedido">{numOrder}</Descriptions.Item>
-          <Descriptions.Item label="Fecha Pedido">{dateOrder}</Descriptions.Item>
-          <Descriptions.Item label="Estado">{stateOrder}</Descriptions.Item>
-          <Descriptions.Item label="Fecha Modificación">{dateModOrder}</Descriptions.Item>
-          <Descriptions.Item label="Tipo">{typeOrder} </Descriptions.Item>
-          <Descriptions.Item label="Cod. Campaña">{codDiscountOrder} </Descriptions.Item>
-        </Descriptions>
-        </DescriptionContainer>
-        <TableContainer>
+}) => {
+    const disable = detailOrder.length < 2 || loading || ( codestadoOrder != 'completed' && codestadoOrder != 'retained' )
+    return (
+        <div>
+            <InfoContainer>
+                <DescriptionContainer>
+                    <Descriptions title="Datos Pedido" layout="horizontal">
+                        <Descriptions.Item label="Num. Pedido">{numOrder}</Descriptions.Item>
+                        <Descriptions.Item label="Fecha Pedido">{dateOrder}</Descriptions.Item>
+                        <Descriptions.Item label="Estado">{stateOrder}</Descriptions.Item>
+                        <Descriptions.Item label="Fecha Modificación">{dateModOrder}</Descriptions.Item>
+                        <Descriptions.Item label="Tipo">{typeOrder} </Descriptions.Item>
+                        <Descriptions.Item label="Cod. Campaña">{codDiscountOrder} </Descriptions.Item>
+                    </Descriptions>
+                </DescriptionContainer>
+                <TableContainer>
 
-      <Table dataSource= {detailOrder} pagination= {false}>
-          {/* <Column
+                    <Table dataSource= {detailOrder} pagination= {false}>
+                        {/* <Column
             title="Item"
             dataIndex="idpedido"
             key="idpedido"
           /> */}
 
-          <Column
-            title="Cod. Nacional"
-            dataIndex="codnacional"
-            key="codnacional"
-          />
+                        <Column
+                            title="Cod. Nacional"
+                            dataIndex="codnacional"
+                            key="codnacional"
+                        />
 
-          <Column
-            title="Nombre Producto"
-            dataIndex="nombre"
-            key="nombre"
-          />
+                        <Column
+                            title="Nombre Producto"
+                            dataIndex="nombre"
+                            key="nombre"
+                        />
 
-          <Column
-            title="Unidades"
-            dataIndex="cantidad"
-            key="cantidad"
-          />
+                        <Column
+                            title="Unidades"
+                            dataIndex="cantidad"
+                            key="cantidad"
+                        />
 
 
 
-          <Column
-            title="Descuento"
-            dataIndex="descuento"
-            key="descuento"
-          />
+                        <Column
+                            title="Descuento"
+                            dataIndex="descuento"
+                            key="descuento"
+                        />
 
-        {typeOrder === 'Pedidos'?
-          <Column
-          title="Puntos Acomulados"
-          dataIndex="puntos_acumulados_unidad"
-          key="puntos_acumulados_unidad"
-        />
-        :''}
+                        {typeOrder === 'Pedidos'?
+                            <Column
+                                title="Puntos Acomulados"
+                                dataIndex="puntos_acumulados_unidad"
+                                key="puntos_acumulados_unidad"
+                            />
+                            :''}
 
-        {typeOrder === 'Puntos'?
-        <Column
-          title="Puntos Coste"
-          dataIndex="puntos_coste_unidad"
-          key="puntos_coste_unidad"
-        />
-        :''}
-          { detailOrder &&
-          <Column
-              title="Acciones"
-              dataIndex="delete"
-              key="delete"
-              render={ (text, row) => (
-                  <Popconfirm
-                      placement="topLeft"
-                      title={`Se va a proceder a la anulación de la línea del producto ${row.nombre}`}
-                      onConfirm={() => {
-                          deleteOrderLineSetLoading({ id: row.idproducto })
-                          deleteOrderLineById({ idproducto: row.idproducto, idpedido: row.idpedido   })
-                      }}
-                      okText="Confirmar"
-                      cancelText="Cancelar"
-                  >
-                      <Button disabled={ detailOrder.length < 2 || loading || ( codestadoOrder != 'completed' && codestadoOrder != 'retained' )} className="ant-btn-dangerous" danger style={{marginTop: '10px'}}>{ loading == row.idproducto ? <LoadingOutlined /> : <DeleteOutlined /> }</Button>
-                  </Popconfirm>)
-              }
-          />
-          }
+                        {typeOrder === 'Puntos'?
+                            <Column
+                                title="Puntos Coste"
+                                dataIndex="puntos_coste_unidad"
+                                key="puntos_coste_unidad"
+                            />
+                            :''}
+                        { detailOrder &&
+                        <Column
+                            title="Acciones"
+                            dataIndex="delete"
+                            key="delete"
+                            render={ (text, row) => {
+                                if ( disable ) {
+                                    return (
+                                        <Tooltip title="Sólo se pueden modificar pedidos en estado 'Tramitado' y 'Retenido'">
+                                            <Button disabled={ true } className="ant-btn-dangerous" danger style={{marginTop: '10px'}}>{ loading == row.idproducto ? <LoadingOutlined /> : <DeleteOutlined /> }</Button>
+                                        </Tooltip>
+                                        );
+                                }
+                                return (
+                                    <Popconfirm
+                                        placement="topLeft"
+                                        title={`Se va a proceder a la anulación de la línea del producto ${row.nombre}`}
+                                        onConfirm={() => {
+                                            deleteOrderLineSetLoading({ id: row.idproducto })
+                                            deleteOrderLineById({ idproducto: row.idproducto, idpedido: row.idpedido   })
+                                        }}
+                                        okText="Confirmar"
+                                        cancelText="Cancelar"
+                                    >
+                                        <Button className="ant-btn-dangerous" danger style={{marginTop: '10px'}}>{ loading == row.idproducto ? <LoadingOutlined /> : <DeleteOutlined /> }</Button>
+                                    </Popconfirm>);
+                                }
+                            }
+                        />
+                        }
 
-        </Table>
-        </TableContainer>
-      </InfoContainer>
+                    </Table>
+                </TableContainer>
+            </InfoContainer>
 
-    </div>
-  )
+        </div>
+    )
+}
 
 
 
@@ -131,7 +143,6 @@ deleteOrderLineSetLoading,
 
 
 InfoCardOrder.propTypes = {
-  logout: PropTypes.func.isRequired,
   status: PropTypes.string.isRequired,
   history: PropTypes.shape({}).isRequired,
   location: PropTypes.shape({
--- src/components/ModalDetailOrder/view.js
+++ src/components/ModalDetailOrder/view.js
@@ -1,41 +1,53 @@
 
 import React from 'react';
 import 'antd/dist/antd.css';
-import {Button, Modal, Popconfirm} from 'antd';
+import {Button, Modal, Popconfirm, Tooltip} from 'antd';
 import ButtonGroup from "antd/lib/button/button-group";
-import {LoadingOutlined} from "@ant-design/icons";
+import {DeleteOutlined, LoadingOutlined} from "@ant-design/icons";
 
 const ModalDetailOrder =({ visibility, ok, cancel, customFooter, content, deleteOrderById, order, entity, client, deleteOrderSetLoading }) =>
-  (
-      <div>
-        <Modal
-          title="Detalles"
-          visible={visibility}
-          destroyOnClose={true}
-          onOk={ok}
-          onCancel={cancel}
-          footer={customFooter}
-          width= {900}
-          bodyStyle = {{height: '600px'}} 
-        >{content}
-            { order ? (
-                <Popconfirm
-                    placement="topLeft"
-                    title={`Se va a proceder a la anulación del pedido ${order.idpedido} de la entidad ${order.nomentidad_cbim}.`}
-                    onConfirm={() => {
-                        deleteOrderSetLoading({});
-                        deleteOrderById({ id: order.idpedido})
-                    }}
-                    okText="Confirmar"
-                    cancelText="Cancelar"
-                >
-                <Button className="ant-btn-dangerous" danger style={{marginTop: '10px'}} disabled={order && order.loadingDelete}>{ order && order.loadingDelete ? (<LoadingOutlined />) : 'Anular Pedido' }</Button>
-            </Popconfirm>) : ''}
-            <span style={{color: 'red', marginLeft: '10px'}} hidden={!(order && order.error)}>{order && order.error ? order.error : ''}</span>
+{
+    const disable = order && order.loadingDelete || order && ( order.codestado != "completed" || order.codestado != "retained" );
 
-        </Modal>
-      </div>
-    )
+    const actions =  order && ( disable ? (
+        <Tooltip title="Sólo se pueden modificar pedidos en estado 'Tramitado' y 'Retenido'">
+            <Button className="ant-btn-dangerous" style={{marginTop: '10px'}} disabled={ true }>{ order && order.loadingDelete ? (<LoadingOutlined />) : 'Anular Pedido' }</Button>
+        </Tooltip>
+    ) : (
+        <Popconfirm
+            placement="topLeft"
+            title={`Se va a proceder a la anulación del pedido ${order.idpedido} de la entidad ${order.nomentidad_cbim}.`}
+            onConfirm={() => {
+                deleteOrderSetLoading({});
+                deleteOrderById({ id: order.idpedido})
+            }}
+            okText="Confirmar"
+            cancelText="Cancelar"
+        >
+            <Button className="ant-btn-dangerous" danger style={{marginTop: '10px'}} disabled={order && order.loadingDelete || order && ( order.codestado != "completed" || order.codestado != "retained" )}>{ order && order.loadingDelete ? (<LoadingOutlined />) : 'Anular Pedido' }</Button>
+        </Popconfirm>) );
+
+    return (
+        <div>
+            <Modal
+                title="Detalles"
+                visible={visibility}
+                destroyOnClose={true}
+                onOk={ok}
+                onCancel={cancel}
+                footer={customFooter}
+                width= {900}
+                bodyStyle = {{height: '600px'}}
+            >{content}
+                {actions}
+                <span style={{color: 'red', marginLeft: '10px'}} hidden={!(order && order.error)}>{order && order.error ? order.error : ''}</span>
+
+            </Modal>
+        </div>
+    );
+
+}
+
   
 
 ModalDetailOrder.propTypes = {
